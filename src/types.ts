e80f1e479d27a5d6b3600f0ccdf6a65bc39eaa12
lucasdafialho <lucas.ffialho06@gmail.com>
2024-09-16 18:43:23 -0300

    Update Types

---
+++ src/types.ts
@@ -0,0 +1,17 @@
+export type CardsFeedProps = {
+    icon: string;
+    description: string;
+    name: string;
+    author: string;
+}
+
+export type AccordionProps = {
+    title: string;
+}
+
+export type MembersCardsProps = {
+    img: string;
+    name:string;
+    rm:number;
+    link: string;
+}
